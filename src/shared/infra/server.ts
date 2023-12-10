66a1a13dfcb91737b1c1225792a8115e0c6a025b
JGGrande <joaogabriel7303@gmail.com>
2023-12-10 12:48:11 -0300

    Implementado novas configurações de loadbalance

--- src/shared/infra/server.ts
+++ src/shared/infra/server.ts
@@ -5,7 +5,7 @@ import AppError from "../errors/AppError";
 import { env } from "../env/index"
 import { routes } from './routes';
 import cluster from "cluster";
-import { cpus } from "os";
+import { cpus, hostname } from "os";
 
 const numberOfCPUs = cpus().length;
 
@@ -32,7 +32,7 @@ if(cluster.isPrimary){
 
   app.get('/', (request: Request, response: Response) => {
     console.log(process.pid)
-    return response.end(process.ppid.toString())
+    return response.json({pid: process.pid.toString(), hostname: hostname() })
   })
 
   app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
@@ -51,8 +51,7 @@ if(cluster.isPrimary){
       }
 
       return response.status(500).send({ message: 'Erro interno no servidor.' })
-  },
-  );
+  });
 
 
   app.listen(env.PORT, () => {
@@ -60,3 +59,39 @@ if(cluster.isPrimary){
       console.log(`Server is running on port ------>>>>>> ${env.PORT}`)
   })
 }
+
+// const app = express()
+
+// app.use(express.json({ limit: '5mb'}));
+
+// app.use(routes);
+
+// app.get('/', (request: Request, response: Response) => {
+//   console.log(process.pid)
+//   return response.json({pid: process.pid.toString(), hostname: hostname() })
+// })
+
+// app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+
+//     if (err instanceof AppError) {
+//         return response.status(err.statusCode).json({
+//             status: 'error',
+//             message: err.message,
+//         });
+//     }
+
+//     if (env.NODE_ENV != 'production') {
+//         console.error(err)
+//     } else {
+//         //colocar para enviar logs para Datadog,NewRelic ou Sentry
+//     }
+
+//     return response.status(500).send({ message: 'Erro interno no servidor.' })
+// },
+// );
+
+
+// app.listen(env.PORT, () => {
+//     console.log(process.pid)
+//     console.log(`Server is running on port ------>>>>>> ${env.PORT}`)
+// })
